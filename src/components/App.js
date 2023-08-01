c83af3fbc997fa75d09d184311387e34e91c6278
Jay Lim <jlimoman@gmail.com>
2023-08-01 00:36:40 -0600

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+import LandingPage from './LandingPage';
+import { Routes, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <body>
+      <h1>Landing Page</h1>
+
+    </body>
+  )
+}
+
+export default LandingPage;
